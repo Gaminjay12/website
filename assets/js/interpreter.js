ad5aebeeb913a3c14c1c6414fedbc32fc7980be4
The Jay <mastercrafterhayden@outlook.com>
2025-09-10 04:36:13 +0000

    hopr

--- assets/js/interpreter.js
+++ assets/js/interpreter.js
@@ -1,8 +1,8 @@
 let textarea = document.getElementById('js-textarea');
 
 function RunJS() {
-    console.log(textareaOutput);
-    alert(textareaOutput);
     let textareaContent = textarea.value;
     let textareaOutput = eval(textareaContent);
+    console.log(textareaOutput);
+    alert(textareaOutput);
 }
